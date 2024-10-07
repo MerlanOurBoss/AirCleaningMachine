04979a2c7515c898f75e96b891143c241d5df921
Yermukhammed Assankhan <merlan01tel@gmail.com>
2024-10-07 13:59:04 +0500

    init

--- Assets/Scripts/UI/SxemaCO2.cs
+++ Assets/Scripts/UI/SxemaCO2.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class SxemaCO2 : MonoBehaviour
@@ -9,10 +10,40 @@ public class SxemaCO2 : MonoBehaviour
     [SerializeField] private GameObject sxema;
     [SerializeField] private GameObject sxemaOriginal;
 
+    [SerializeField] private TextMeshProUGUI[] gazValue;
+    [SerializeField] private TextMeshProUGUI[] manValue;
+    private float gaz1 = 0;
+    private float gaz2 = 0;
+    private float gaz3 = 0;
+
+    private float man1 = 0;
+    private float man2 = 0;
+    private float man3 = 0;
+
+    private bool ifFirstGazeOpen = false;
+    private bool ifSecondGazeOpen = false;
+    private bool ifThirdGazeOpen = false;
+    private bool ifSecondGazeOpenCO2 = false;
+    private bool ifThirdGazeOpenCO2 = false;
+
+    private bool ifFirstManOpen = false;
+    private bool ifSecondManOpen = false;
+    private bool ifThirdManOpen = false;
+
     private bool isOff = false;
 
     private bool isActivated = false;
 
+    public void Start()
+    {
+        gazValue[0].text = "0.0";
+        gazValue[1].text = "0.0";
+        gazValue[2].text = "0.0";
+
+        manValue[0].text = "0.0";
+        manValue[1].text = "0.0";
+        manValue[2].text = "0.0";
+    }
     public void OnOffSxema()
     {
         if (!isActivated)
@@ -34,15 +65,147 @@ public class SxemaCO2 : MonoBehaviour
 
     private void Update()
     {
+        Debug.Log("ifFirstGazeOpen = " + ifFirstGazeOpen);
+        gazValue[0].text = gaz1.ToString("0.0");
+        gazValue[1].text = gaz2.ToString("0.0");
+        gazValue[2].text = gaz3.ToString("0.0");
+
+        manValue[0].text = man1.ToString("0.0");
+        manValue[1].text = man2.ToString("0.0");
+        manValue[2].text = man3.ToString("0.0");
+
         if (isOff)
         {
             sxemSborCO2Anim.Play("LinesStopSbor");
             isOff = false;
         }
+
+        if (ifFirstManOpen)
+        {
+            man1 = Mathf.Lerp(man1, 1800f, 3 * Time.deltaTime);
+            if (man1 >= 1799.99)
+            {
+                ifFirstManOpen = false;
+            }
+        }
+
+        if (ifSecondManOpen)
+        {
+            man2 = Mathf.Lerp(man2, 1800f, 3 * Time.deltaTime);
+            if (man2 >= 1799.99)
+            {
+                ifSecondManOpen = false;
+            }
+        }
+
+        if (ifThirdManOpen)
+        {
+            man3 = Mathf.Lerp(man3, 1800f, 3 * Time.deltaTime);
+            if (man3 >= 1799.99)
+            {
+                ifThirdManOpen = false;
+            }
+        }
+
+        if (ifFirstGazeOpen)
+        {
+            gaz1 = Mathf.Lerp(gaz1, 1000f, 3 * Time.deltaTime);
+            if (gaz1 >= 999.99)
+            {
+                ifFirstGazeOpen = false;
+            }
+        }
+
+        if (ifSecondGazeOpen)
+        {
+            gaz2 = Mathf.Lerp(gaz2, 100f, 2 * Time.deltaTime);
+            if (gaz2 >= 99.99)
+            {
+                ifSecondGazeOpen = false;
+            }
+        }
+
+        if (ifThirdGazeOpen)
+        {
+            gaz3 = Mathf.Lerp(gaz3, 100f, 2 * Time.deltaTime);
+            if (gaz3 >= 99.99)
+            {
+                ifThirdGazeOpen = false;
+            }
+        }
+
+        if (ifSecondGazeOpenCO2)
+        {
+            gaz2 = Mathf.Lerp(gaz2, 1000f, 3 * Time.deltaTime);
+            if (gaz2 >= 999.99)
+            {
+                ifSecondGazeOpenCO2 = false;
+            }
+        }
+
+        if (ifThirdGazeOpenCO2)
+        {
+            gaz3 = Mathf.Lerp(gaz3, 1000f, 3 * Time.deltaTime);
+            if (gaz3 >= 999.99)
+            {
+                ifThirdGazeOpenCO2 = false;
+            }
+        }
     }
     public void SxemaStart()
     {
         sxemSborCO2Anim.Play("LinesZeroAction");
+    }
+
+    public void StartFirstGaz()
+    {
+        ifFirstGazeOpen = true;
+    }
+
+    public void StartSecondGaz()
+    {
+        ifSecondGazeOpen = true;
+    }    
+    
+    public void StartThirdGaz()
+    {
+        ifThirdGazeOpen = true;
+    }
+
+    public void StartSecondGazCO2()
+    {
+        ifSecondGazeOpenCO2 = true;
+    }
+
+    public void StartThirdGazCO2()
+    {
+        ifThirdGazeOpenCO2 = true;
+    }
+
+    public void StartFisrtMan()
+    {
+        ifFirstManOpen = true;
+    }
+
+    public void StartSecondMan()
+    {
+        ifSecondManOpen = true;
+    }
+
+    public void StartThirdMan()
+    {
+        ifThirdManOpen = true;
+    }
+
+    public void ResetGaz()
+    {
+        Debug.Log("dadadadasd");
+        gaz1 = 0;
+        gaz2 = 0;
+        gaz3 = 0;
+        man1 = 0;
+        man2 = 0;
+        man3 = 0;
     }
 
     public void SxemaStart2()
